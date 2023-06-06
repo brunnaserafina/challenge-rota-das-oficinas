50538439800e3d69a40b1ac8bebaeebca9da81e0
Brunna Serafina <brunnaserafina@gmail.com>
2023-06-06 11:29:41 -0300

    feat: implemented routes for Roman Numeral Converter and Restaurant Bill Splitter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RestaurantBillSplitter from "./pages/RestaurantBillSplitter";
 import RomanNumeralConverter from "./pages/RomanNumeralConverter";
 
 export default function App() {
   return (
-    <div>
-      <RomanNumeralConverter />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/roman-numeral-converter"
+          element={<RomanNumeralConverter />}
+        ></Route>
+
+        <Route
+          path="/restaurant-bill"
+          element={<RestaurantBillSplitter />}
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
